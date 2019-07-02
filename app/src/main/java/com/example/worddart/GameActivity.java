4d4df7c98c329ff78b24905648d9b00a8f496a49
Michael <michaelkushnir123233@gmail.com>
2019-07-02 18:50:05 +0300

    added timer

--- app/src/main/java/com/example/worddart/GameActivity.java
+++ app/src/main/java/com/example/worddart/GameActivity.java
@@ -1,14 +1,22 @@
 package com.example.worddart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.dynamicanimation.animation.DynamicAnimation;
+import androidx.dynamicanimation.animation.FlingAnimation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import org.w3c.dom.Text;
 
@@ -22,9 +30,13 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     Intent intent;
     String[] Mode;
     String MODE_OFFLINE,MODE_ONLINE,MODE_SOLO,MODE_AI,MODE_TIMED,MODE_ELIMINATION;
-    Button btnAdd,btnSub;
+    final long INTERVAL_MILLIS=5*60*1000, MAX_MILLIS=85*60*1000,MIN_MILLIS=INTERVAL_MILLIS;
     TextView tvTimer,tvMode;
-
+    Button btnAdd,btnSub,btnStart;
+    Animation fadeOut;
+    CountDownTimer timer;
+    long timeLeftInMillis;
+    Boolean timerIsRun;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,13 +51,17 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         MODE_AI = getResources().getResourceName(R.string.OFFLINE_AI_MODE);
         MODE_TIMED = getResources().getResourceName(R.string.GAMEMODE_TIMED);
         MODE_ELIMINATION = getResources().getResourceName(R.string.GAMEMODE_ELIMINATION);
+        timeLeftInMillis=0;
         tvTimer=(TextView)findViewById(R.id.tvTimer);
         tvMode=(TextView)findViewById(R.id.TITLE_MODE);
         tvMode.setText(Mode[2]);
         btnAdd=(Button)findViewById(R.id.btAdd);
         btnSub=(Button)findViewById(R.id.btSub);
+        btnStart=(Button)findViewById(R.id.btnStart);
         btnAdd.setOnClickListener(this);
         btnSub.setOnClickListener(this);
+        btnStart.setOnClickListener(this);
+        fadeOut= AnimationUtils.loadAnimation(this,R.anim.fadeout);
     }
 
     @Override
@@ -83,26 +99,84 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        int minutes;
-        String setTime;
+
         switch (view.getId()){
             case R.id.btAdd:
-                minutes=Integer.parseInt(tvTimer.getText().toString().split("m")[0]);
-                if(minutes<=85)
-                minutes+=5;
-                setTime=String.format(Locale.getDefault(),"%02d"+"m "+"%02d"+"s", minutes, 0);
-                tvTimer.setText(setTime);
+                if(timeLeftInMillis<=MAX_MILLIS)
+                timeLeftInMillis+=INTERVAL_MILLIS;
+                updateTimer();
                 break;
             case R.id.btSub:
-                minutes=Integer.parseInt(tvTimer.getText().toString().split("m")[0]);
-                if(minutes>=5)
-                minutes-=5;
-                setTime=String.format(Locale.getDefault(),"%02d"+"m "+"%02d"+"s", minutes, 0);
-                tvTimer.setText(setTime);
+                if(timeLeftInMillis>MIN_MILLIS)
+                    timeLeftInMillis-=INTERVAL_MILLIS;
+                updateTimer();
+                break;
+            case R.id.btnStart:
+                AnimateFadeOut();
+                startTimer();
                 break;
         }
     }
+public void AnimateFadeOut()
+{
+    FlingAnimation fling = new FlingAnimation(btnStart, DynamicAnimation.TRANSLATION_Y);
+    btnStart.startAnimation(fadeOut);
+    btnSub.startAnimation(fadeOut);
+    btnAdd.startAnimation(fadeOut);
+    fling.setStartVelocity(2000)
+            .setMaxValue(2000)
+            .setMinValue(-2000)
+            .setFriction(0.3f)
+            .start();
+    Snackbar.make(findViewById(R.id.GameLayout), R.string.StartMessage,
+            Snackbar.LENGTH_LONG)
+            .show();
 
+}
+
+public void startCountDown()
+{
+    timeLeftInMillis=Integer.parseInt(tvTimer.getText().toString().split("m")[0])*60*1000;
+    timer=new CountDownTimer(timeLeftInMillis+1,1000) {
+        @Override
+        public void onTick(long l) {
+            timeLeftInMillis=l;
+            updateTimer();
+        }
+
+        @Override
+        public void onFinish() {
+
+        }
+    }.start();
+    timerIsRun=true;
+}
+public void stopTimer()
+{
+    timer.cancel();
+    timerIsRun=false;
+}
+public void updateTimer()
+{
+    int minutes= (int)timeLeftInMillis/60000;
+    int seconds=(int)timeLeftInMillis%60000/1000;
+    String setTime=String.format(Locale.getDefault(),"%02d"+"m "+"%02d"+"s", minutes, seconds);
+    tvTimer.setText(setTime);
+}
+//Used for delaying the timer, giving the user some time to get ready
+    private void startTimer(){
+        timer=new CountDownTimer(4000,10) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                startCountDown();
+            }
+        }.start();
+    }
 
 }
 
