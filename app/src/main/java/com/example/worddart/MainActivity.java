9f7cc73bb17c7f64327e2f9c43252095c2b71d74
Michael <michaelkushnir123233@gmail.com>
2019-09-11 18:07:50 +0300

    Added user insertion to db; Added connect function and create lobby function;

--- app/src/main/java/com/example/worddart/MainActivity.java
+++ app/src/main/java/com/example/worddart/MainActivity.java
@@ -1,23 +1,30 @@
 package com.example.worddart;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ShareCompat;
 import androidx.core.content.ContextCompat;
 import androidx.preference.PreferenceManager;
 
-import android.app.IntentService;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -28,14 +35,17 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
-import java.util.Objects;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.UnsupportedEncodingException;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
-    private static final int RC_SIGN_IN =9000 ;
+    private static final int RC_SIGN_IN = 9000;
     private static final String TAG = "STATUS_MAIN";
-    Button btnPlay,btnStats,btnSettings;
+    final String DatabaseURL="https://word-dart.herokuapp.com";
     SignInButton btnGoogle;
     TextView dispName;
     Intent intent;
@@ -43,19 +53,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     CircleImageView profile;
     SharedPreferences sp;
     SharedPreferences.Editor editor;
+    Button btnPlay, btnStats, btnSettings;
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intent=new Intent();
+        intent = new Intent();
 
-        profile=(CircleImageView) findViewById(R.id.circleImageView);
-        dispName=(TextView)findViewById(R.id.dispName);
-        btnPlay=(Button)findViewById(R.id.btnPlay);
-        btnStats=(Button)findViewById(R.id.btnStats);
-        btnSettings=(Button)findViewById(R.id.btnSettings);
-        btnGoogle=(SignInButton)findViewById(R.id.sign_in_button);
+        profile = (CircleImageView) findViewById(R.id.circleImageView);
+        dispName = (TextView) findViewById(R.id.dispName);
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnStats = (Button) findViewById(R.id.btnStats);
+        btnSettings = (Button) findViewById(R.id.btnSettings);
+        btnGoogle = (SignInButton) findViewById(R.id.sign_in_button);
 
         btnPlay.setOnClickListener(this);
         btnStats.setOnClickListener(this);
@@ -66,8 +78,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnStats.setOnTouchListener(this);
         btnSettings.setOnTouchListener(this);
 
-        sp= PreferenceManager.getDefaultSharedPreferences(this);
-        editor=sp.edit();
+        sp = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = sp.edit();
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
@@ -78,6 +90,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Glide.with(this).load(ContextCompat.getDrawable(this, R.drawable.proficon_crab)
         ).apply(RequestOptions.circleCropTransform()).into(profile);
+
+        queue = Volley.newRequestQueue(this);
+        if (sp.getString("TokenID", null)==null)
+            createUser();
+            Log.d(TAG,"no user found, creating new one");
     }
 
     @Override
@@ -102,30 +119,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
-        if(MotionEvent.ACTION_DOWN==motionEvent.getAction()) {
+        if (MotionEvent.ACTION_DOWN == motionEvent.getAction()) {
             switch (view.getId()) {
                 case R.id.btnPlay:
-                    btnPlay.setBackground(ContextCompat.getDrawable(this,R.drawable.button_bg_rounded_secondary));
+                    btnPlay.setBackground(ContextCompat.getDrawable(this, R.drawable.button_bg_rounded_secondary));
                     break;
                 case R.id.btnStats:
-                    btnStats.setBackground(ContextCompat.getDrawable(this,R.drawable.button_bg_rounded_secondary));
+                    btnStats.setBackground(ContextCompat.getDrawable(this, R.drawable.button_bg_rounded_secondary));
                     break;
                 case R.id.btnSettings:
-                    btnSettings.setBackground(ContextCompat.getDrawable(this,R.drawable.button_bg_rounded_secondary));
+                    btnSettings.setBackground(ContextCompat.getDrawable(this, R.drawable.button_bg_rounded_secondary));
                     break;
             }
-        }
-        else
-        {
+        } else {
             switch (view.getId()) {
                 case R.id.btnPlay:
-                    btnPlay.setBackground(ContextCompat.getDrawable(this,R.drawable.button_bg_rounded_primary));
+                    btnPlay.setBackground(ContextCompat.getDrawable(this, R.drawable.button_bg_rounded_primary));
                     break;
                 case R.id.btnStats:
-                    btnStats.setBackground(ContextCompat.getDrawable(this,R.drawable.button_bg_rounded_primary));
+                    btnStats.setBackground(ContextCompat.getDrawable(this, R.drawable.button_bg_rounded_primary));
                     break;
                 case R.id.btnSettings:
-                    btnSettings.setBackground(ContextCompat.getDrawable(this,R.drawable.button_bg_rounded_primary));
+                    btnSettings.setBackground(ContextCompat.getDrawable(this, R.drawable.button_bg_rounded_primary));
                     break;
             }
         }
@@ -153,7 +168,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
+            updateUserInfo(account);
             // Signed in successfully, show authenticated UI.
             updateUI(account);
         } catch (ApiException e) {
@@ -164,17 +179,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public void updateUI(GoogleSignInAccount account)
-    {
-        if(account==null)
-        {
+    public void updateUI(GoogleSignInAccount account) {
+        if (account == null) {
             btnGoogle.setVisibility(View.VISIBLE);
-        }
-        else
-        {
-            if(account.getPhotoUrl()!=null)
-            editor.putString("ImageURL", account.getPhotoUrl().toString());
-            editor.putString("profName",account.getDisplayName());
+        } else {
+            if (account.getPhotoUrl() != null)
+                editor.putString("ImageURL", account.getPhotoUrl().toString());
+            editor.putString("profName", account.getDisplayName());
             editor.apply();
             profile.setImageURI(account.getPhotoUrl());
             profile.setBorderColor(Color.WHITE);
@@ -183,4 +194,110 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    public void createUser() {
+        final String mRequestBody = "{\"userType\":\"anonymous\",\"fullName\":\"Random_Crab\",\"emailAddress\":\"\"}";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST, DatabaseURL+"/api/users/insert", (JSONArray) null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.i("LOG_RESPONSE", response.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("LOG_RESPONSE", error.toString());
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() {
+                try {
+                    return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
+                    return null;
+                }
+            }
+
+            @Override
+            protected Response<JSONArray> parseNetworkResponse(NetworkResponse response) {
+                JSONArray jsonArray = new JSONArray();
+                if (response != null) {
+                    jsonArray.put(String.valueOf(response.statusCode));
+
+                    String tokenid = "";
+                    try {
+                        tokenid = new String(response.data, "UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    Log.d(TAG, "RES_BODY: " + tokenid);
+                    tokenid = tokenid.substring(tokenid.indexOf("_id") + 6, tokenid.indexOf("userType") - 3);
+                    Log.d(TAG, "tokenid: " + tokenid);
+                    editor.putString("TokenID", tokenid);
+                    editor.apply();
+                }
+                return Response.success(jsonArray, HttpHeaderParser.parseCacheHeaders(response));
+            }
+        };
+        queue.add(jsonArrayRequest);
+    }
+
+    public void updateUserInfo(GoogleSignInAccount account)
+    {
+        final String mRequestBody = "{\"userType\":\"google\",\"fullName\":\""+account.getDisplayName()+"\",\"emailAddress\":\""+account.getEmail()+"\"}";
+        String tokenid=sp.getString("TokenID",null);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, DatabaseURL+"/api/users/update?userID="+tokenid, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.i("LOG_RESPONSE", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("LOG_RESPONSE", error.toString());
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
+                    return null;
+                }
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                String responseString = "";
+                if (response != null) {
+                    responseString = String.valueOf(response.statusCode);
+
+                    String tokenid = null;
+                    try {
+                        tokenid = new String(response.data, "UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    tokenid = tokenid.substring(tokenid.indexOf("_id") + 6, tokenid.indexOf("userType") - 3);
+                    Log.d(TAG, "tokenid: " + tokenid);
+                    editor.putString("TokenID", tokenid);
+                    editor.apply();
+                }
+                return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+            }
+        };
+        queue.add(stringRequest);
+    }
 }
--- app/src/main/java/com/example/worddart/PlayMenu.java
+++ app/src/main/java/com/example/worddart/PlayMenu.java
@@ -3,16 +3,35 @@ package com.example.worddart;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.widget.TextViewCompat;
+import androidx.preference.PreferenceManager;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
@@ -20,7 +39,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
+import java.util.Map;
 
 public class PlayMenu extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
     Button[] btn;
@@ -29,10 +50,15 @@ public class PlayMenu extends AppCompatActivity implements View.OnClickListener,
     Intent intent;
     String[] Mode;
     TextView tvOnline,tvOffline;
+    final String DatabaseURL="https://word-dart.herokuapp.com";
+    SharedPreferences sp;
+    private RequestQueue queue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_menu);
+        sp= PreferenceManager.getDefaultSharedPreferences(this);
         tvOffline=(TextView)findViewById(R.id.Offline);
         tvOnline=(TextView)findViewById(R.id.multi);
         intent=new Intent();
@@ -47,7 +73,7 @@ public class PlayMenu extends AppCompatActivity implements View.OnClickListener,
         }
         TextViewCompat.setAutoSizeTextTypeWithDefaults(tvOnline,TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM);
         TextViewCompat.setAutoSizeTextTypeWithDefaults(tvOffline,TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM);
-
+        queue = Volley.newRequestQueue(this);
     }
 
     @Override
@@ -55,10 +81,16 @@ public class PlayMenu extends AppCompatActivity implements View.OnClickListener,
     switch(view.getId())
     {
         case R.id.btn0:
+            //QuickLobby();
             break;
         case R.id.btn1:
+            //FindLobby();
             break;
         case R.id.btn2:
+            String data=getAvailable();
+            if(data==null)
+                CreateLobby();
+            else ConnectLobby(data);
             break;
         case R.id.btn3:
             Mode[0]=getResources().getString(R.string.OFFLINE_MODE);
@@ -118,6 +150,136 @@ public class PlayMenu extends AppCompatActivity implements View.OnClickListener,
         ibElimination.setOnClickListener(this);
         d.show();
 
+    }
+    // TODO make a helper class that deals with all of these database-centred functions - pack it up nicely
+    public String getAvailable()
+    {
+        final String url = DatabaseURL+"/api/lobbies/getAvailable";
+        final String[] obj = {null};
+// prepare the Request
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, (String) null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // display response
+                        Log.d("Response", response.toString());
+                        obj[0] =response.toString();
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response",error.toString());
+
+                    }
+                }
+        );
+        queue.add(getRequest);
+        return obj[0];
+    }
+    public void ConnectLobby(String lobby_id)
+    {
+        String url=DatabaseURL;
+        String body;
+        if(lobby_id!=null)
+        {
+                String tokenid=sp.getString("TokenID",null);
+                Log.d("lobbyID", "CreateLobby: id - "+lobby_id);
+                url += "/api/lobbies/connect?lobbyID="+lobby_id+"&userID="+tokenid;
+                StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>()
+                        {
+                            @Override
+                            public void onResponse(String response) {
+                                // response
+                                Log.d("Response", response);
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // error
+                                Log.d("Error.Response", error.toString());
+                            }
+                        }
+                ) {
+                    @Override
+                    protected Map<String, String> getParams()
+                    {
+                        Map<String, String> params = new HashMap<>();
+                        return params;
+                    }
+                };
+                queue.add(postRequest);
+
+        }
+    }
+    public void CreateLobby()
+    {
+        String url=DatabaseURL;
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("title", "Lobby");
+            jsonBody.put("isAvailable", true);
+            jsonBody.put("playerCount",0);
+            jsonBody.put("playerArr",new JSONArray());
+            jsonBody.put("currentWord","");
+            final String mRequestBody = jsonBody.toString();
+            url+="/api/lobbies/insert";
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("LOG_RESPONSE", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("LOG_RESPONSE", error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+
+                        String tokenid = "";
+                        try {
+                            tokenid = new String(response.data, "UTF-8");
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                        tokenid = tokenid.substring(tokenid.indexOf("_id") + 6, tokenid.indexOf("title") - 3);
+                        Log.d("debuggo", "token: " + tokenid);
+                        ConnectLobby(tokenid);
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+            };
+
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
 }
